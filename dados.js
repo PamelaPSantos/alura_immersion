748dd58c35b521f9cbaaee7477cc12115b5cf792
Pamela Pereira dos Santos <pamela.santos@samisaude.com>
2024-09-11 14:18:28 -0300

    feat: first commit

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        nome: 'Meredith Grey',
+        descricao: 'Meredith Grey é a protagonista da série médica Greys Anatomy. Desde o início, acompanhamos sua jornada como uma jovem e ambiciosa interna de cirurgia, enfrentando os desafios da vida profissional e pessoal em um hospital. Ao longo da série, Meredith se destaca por sua inteligência, resiliência e coração compassivo.',
+        img: './imgs/meredith.jpg',
+        link: 'https://pt.wikipedia.org/wiki/Meredith_Grey'
+    },
+    {
+        nome: 'Cristina Yang',
+        descricao: 'Cristina Yang é um dos maiores atores da série Greys Anatomy. Atualmente, é uma atriz e um dos principais atores da franquia, e também um dos maiores proprietários do estúdio Grey & Sloan. Yang é uma atriz de múltiplas faixas e tem um caráter rebuscado, que muda constantemente, enquanto se dedica a sua carreira de cirurgia. ',
+        img: './imgs/cristina.jpg',
+        link: 'https://pt.wikipedia.org/wiki/Cristina_Yang'
+    }, 
+    {
+        nome: 'Mark Sloan',
+        descricao: 'Mark Sloan é um dos fundadores da série Greys Anatomy. Ele é um dos principais criadores da franquia, e também um dos maiores proprietários do estúdio Grey & Sloan. Sloan é um atleta profissional e um dos principais fundadores da série, com uma carreira de cirurgia e medicina. ',
+        img: './imgs/mark.jpg',
+        link: 'https://pt.wikipedia.org/wiki/Mark_Sloan'
+    }
+];
